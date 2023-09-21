ae353326e8b6d5c3f8a6e7c91bd025e7ae4903e1
SusanSu123 <suchangau@hotmail.com>
2023-09-21 23:04:15 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
+
+
+
 import streamlit as st
 import pandas as pd
 
@@ -7,9 +10,12 @@ st.title("Data Science App")
 # Define the GitHub URL of the raw dataset file in your repository
 github_url = "https://github.com/SusanSu123/Case-study/blob/b07f3ab3bf24b783fd4219d405927145b8bdaabc/vic_data.xlsx"
 
+# Specify the Excel file format engine (e.g., 'openpyxl' or 'xlrd')
+excel_engine = 'openpyxl'
+
 # Read the dataset from the GitHub URL into a DataFrame
 try:
-    df = pd.read_excel(github_url)
+    df = pd.read_excel(github_url, engine=excel_engine)
     st.write("## Data Preview")
     st.write(df)
 except Exception as e:
