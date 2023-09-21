4548799c0dc9207ce6c8940114c3c35b44ec862d
SusanSu123 <suchangau@hotmail.com>
2023-09-21 22:44:14 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import pandas as pd
 
-st.title("My Streamlit App")
-st.write("Hello, World!")
+# Set the title of your Streamlit app
+st.title("Data Science Prototype")
 
+# Define the path to your local Excel file
+file_path = "/Users/susan/RMIT/3. S2 2023/Case Studies in Data Science/WIL Project/vic_data.xlsx"
+
+# Check if the file exists at the specified path
+if st.button("Load Local Excel File"):
+    try:
+        df = pd.read_excel(file_path)
+        st.success("File loaded successfully!")
+    except Exception as e:
+        st.error(f"An error occurred: {e}")
