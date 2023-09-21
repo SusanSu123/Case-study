c0c5c865d64ef48387c63846333c358c8eb3a90f
SusanSu123 <suchangau@hotmail.com>
2023-09-21 22:47:56 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,10 @@ import pandas as pd
 # Set the title of your Streamlit app
 st.title("Data Science Prototype")
 
-# Define the path to your local Excel file
-file_path = "/Users/susan/RMIT/3. S2 2023/Case Studies in Data Science/WIL Project/vic_data.xlsx"
+# Define the URL of the dataset
+dataset_url = "https://rmiteduau-my.sharepoint.com/:x:/g/personal/s3970670_student_rmit_edu_au/EdR2K2roIBBPqGjHhwiz5XEB1qIy18gyfuZ7BQ_b_XJg0Q?e=AwHIOn"
 
-# Check if the file exists at the specified path
-if st.button("Load Local Excel File"):
-    try:
-        df = pd.read_excel(file_path)
-        st.success("File loaded successfully!")
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+# Read the dataset from the URL into a DataFrame
+df = pd.read_excel(dataset_url)
+st.write("## Data Preview")
+st.write(df)
